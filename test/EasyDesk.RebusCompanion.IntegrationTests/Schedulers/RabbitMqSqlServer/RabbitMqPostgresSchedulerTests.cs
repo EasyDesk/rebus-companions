1edb7676479389ed56fce7e21d35e203e99b7b79
francescodente <dente.francesco1996@gmail.com>
2023-05-24 10:00:10 +0200

    Add tests for rabbitMQ and SqlServer scheduler

---
+++ test/EasyDesk.RebusCompanion.IntegrationTests/Schedulers/RabbitMqSqlServer/RabbitMqPostgresSchedulerTests.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyDesk.RebusCompanion.IntegrationTests.Schedulers.RabbitMqPostgres;
+
+public class RabbitMqSqlServerSchedulerTests : AbstractSchedulerTests<RabbitMqSqlServerSchedulerFixture>
+{
+    public RabbitMqSqlServerSchedulerTests(RabbitMqSqlServerSchedulerFixture fixture) : base(fixture)
+    {
+    }
+}
