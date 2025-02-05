68819c820c39b5adf0b09c080aca71117a53bf17
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2025-02-05 14:16:44 +0100

    Add maildev fixture

---
+++ test/EasyDesk.RebusCompanions.IntegrationTests/Maildev/MaildevClient.cs
@@ -0,0 +1,104 @@
+﻿using EasyDesk.CleanArchitecture.Application.Json;
+using EasyDesk.Commons.Collections;
+using EasyDesk.Commons.Options;
+using System.Text.Json;
+
+namespace EasyDesk.RebusCompanions.IntegrationTests.Maildev;
+
+public class MaildevClient
+{
+    private readonly List<MaildevMail> _readEmails = [];
+
+    private readonly HttpClient _httpClient;
+    private readonly JsonSerializerOptions _jsonOptions;
+
+    public MaildevClient(Uri rootUri, JsonOptionsConfigurator jsonConfigurator)
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = rootUri,
+        };
+        _jsonOptions = jsonConfigurator.CreateOptions();
+    }
+
+    public async Task<IEnumerable<MaildevMail>> GetEmails()
+    {
+        var emails = await RetrieveNewEmails();
+
+        _readEmails.AddRange(emails);
+
+        return _readEmails;
+    }
+
+    private async Task<IEnumerable<MaildevMail>> RetrieveNewEmails()
+    {
+        using var response = await _httpClient.GetAsync("email");
+        using var content = await response.Content.ReadAsStreamAsync();
+        var emails = JsonSerializer.Deserialize<IEnumerable<MaildevMailDto>>(content, _jsonOptions) ?? throw new Exception("Maildev returned null response");
+        var result = new List<MaildevMail>();
+
+        foreach (var email in emails.Skip(_readEmails.Count))
+        {
+            result.Add(await ConvertToMailObject(email));
+        }
+        return result;
+    }
+
+    private async Task<MaildevMail> ConvertToMailObject(MaildevMailDto dto)
+    {
+        var convertedAttachments = new List<MaildevAttachment>();
+        foreach (var attachment in dto.Attachments.Flatten())
+        {
+            convertedAttachments.Add(await ConvertToAttachmentObject(attachment, dto.Id));
+        }
+        return new(
+            From: dto.From.Select(x => x.Address),
+            To: dto.To.Select(x => x.Address),
+            Subject: dto.Subject,
+            Html: dto.Html,
+            Text: dto.Text,
+            Attachments: convertedAttachments);
+    }
+
+    private async Task<MaildevAttachment> ConvertToAttachmentObject(MaildevAttachmentDto dto, string emailId)
+    {
+        using var response = await _httpClient.GetAsync($"email/{emailId}/attachment/{dto.FileName}");
+        var content = await response.Content.ReadAsStringAsync();
+        return new(
+            ContentType: dto.ContentType,
+            FileName: dto.FileName,
+            Content: content);
+    }
+
+    public async Task DeleteAllEmails()
+    {
+        await _httpClient.DeleteAsync("email/all");
+        _readEmails.Clear();
+    }
+
+    private record MaildevMailDto(
+        string Id,
+        IEnumerable<MaildevAccountDto> From,
+        IEnumerable<MaildevAccountDto> To,
+        string Subject,
+        string Html,
+        string Text,
+        Option<IEnumerable<MaildevAttachmentDto>> Attachments);
+
+    private record MaildevAccountDto(string Address);
+
+    private record MaildevAttachmentDto(string ContentType, string FileName);
+}
+
+public record MaildevMail(
+    IEnumerable<string> From,
+    IEnumerable<string> To,
+    string Subject,
+    string Html,
+    string Text,
+    IEnumerable<MaildevAttachment> Attachments);
+
+public record MaildevAttachment(
+    string ContentType,
+    string FileName,
+    string Content);
---
+++ test/EasyDesk.RebusCompanions.IntegrationTests/Maildev/MaildevFixture.cs
@@ -0,0 +1,50 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using EasyDesk.CleanArchitecture.Application.Json;
+
+namespace EasyDesk.RebusCompanions.IntegrationTests.Maildev;
+
+public class MaildevFixture : IAsyncLifetime
+{
+    public const string User = "maildev-user";
+    public const string Password = "maildev.123";
+    private readonly IContainer _maildevContainer;
+
+    public MaildevClient Client { get; private set; } = default!;
+
+    public int Port { get; private set; }
+
+    public MaildevFixture()
+    {
+        _maildevContainer = new ContainerBuilder()
+            .WithHostname("maildev")
+            .WithImage("maildev/maildev:2.1.0")
+            .WithEnvironment(new Dictionary<string, string>()
+            {
+                ["MAILDEV_IP"] = "::",
+                ["MAILDEV_INCOMING_USER"] = User,
+                ["MAILDEV_INCOMING_PASS"] = Password,
+            })
+            .WithPortBinding(1025, true)
+            .WithPortBinding(1080, true)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilContainerIsHealthy())
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _maildevContainer.StartAsync();
+        Client = new MaildevClient(new UriBuilder("http", "localhost", _maildevContainer.GetMappedPublicPort(1080)).Uri, c => JsonDefaults.ApplyDefaultConfiguration(c));
+        Port = _maildevContainer.GetMappedPublicPort(1025);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _maildevContainer.StopAsync();
+    }
+
+    public async Task Reset()
+    {
+        await Client.DeleteAllEmails();
+    }
+}
